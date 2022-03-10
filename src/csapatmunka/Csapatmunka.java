386d13c10d80729fcf5ec9981405da2cd0ccf733
RepkaA <97554747+RepkaA@users.noreply.github.com>
2022-03-10 13:11:22 +0100

    név 2 deklarálása

--- src/csapatmunka/Csapatmunka.java
+++ src/csapatmunka/Csapatmunka.java
@@ -7,6 +7,7 @@ public class Csapatmunka {
     
     public static void main(String[] args) {
         String nev1 = "Ulrich Bence";
+        String nev2 = "Repka András";
         String nev3 = "Rohovszky Ákos";
     }
     
