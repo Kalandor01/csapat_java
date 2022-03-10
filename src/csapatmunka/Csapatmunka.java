00b4af598e195da1806a97309f28d27df38271b4
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-03-10 13:10:35 +0100

    lol

--- src/csapatmunka/Csapatmunka.java
+++ src/csapatmunka/Csapatmunka.java
@@ -6,8 +6,8 @@ public class Csapatmunka {
 
     
     public static void main(String[] args) {
-        String nev3 = "Rohovszky Ákos";
         String nev1 = "Ulrich Bence";
+        String nev3 = "Rohovszky Ákos";
     }
     
 }
